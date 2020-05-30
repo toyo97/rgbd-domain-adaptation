c029dcd2981108659009f638ced45f78a46d4068
Gabriele Goletto <48565490+ezius07@users.noreply.github.com>
2020-05-30 18:11:39 +0200

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,116 @@
+import tqdm
+from torchvision.datasets import VisionDataset
+import glob
+
+from PIL import Image
+
+import os
+import os.path
+import sys
+
+def pil_loader(path):
+
+    with open(path, 'rb') as f:
+
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+class synROD(VisionDataset):
+  
+    def __init__(self, root, RAM, split=None, transform=None, target_transform=None):
+        super(synROD, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        self.images=[]
+        self.RAM=RAM
+        
+        if split not in ["train", "test"]:
+          raise ValueError("Split not acceptable!")
+
+        basename = os.path.join(root,"synROD","synARID_50k-split_")
+        path1 = basename + "depth_" + split + "1.txt"
+        path2 = basename + "rgb_" + split + "1.txt"
+
+        with open(path1, "r") as f1, open(path2, "r") as f2:
+
+          for line1, line2 in zip(f1,f2):
+
+            fields1 = line1.split(" ")
+            fields2 = line2.split(" ")
+            imagedepth = os.path.join(root,"synROD",fields1[0])
+            imageRGB = os.path.join(root,"synROD",fields2[0])
+            
+            if RAM:
+              self.images.append(((pil_loader(imageRGB), pil_loader(imagedepth)), int(fields1[1])))
+            else:
+              self.images.append(((imageRGB,imagedepth),int(fields1[1])))
+
+
+    def __getitem__(self, index):
+
+        images, label = self.images[index] 
+
+        if self.transform is not None:
+
+            if self.RAM:
+              image1 = self.transform(images[0])
+              image2 = self.transform(images[1])
+            else:
+              image1 = self.transform(pil_loader(images[0]))
+              image2 = self.transform(pil_loader(images[1]))
+
+        return image1,image2, label
+
+    def __len__(self):
+
+        length = len(self.images) 
+
+        return length
+        
+class ROD(VisionDataset):
+  
+    def __init__(self, root, RAM, transform=None, target_transform=None):
+        super(ROD, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        self.images=[]
+        self.RAM=RAM
+        
+        with open(os.path.isfile(os.path.join(root,"rod-split_sync.txt"))) as f:
+          for line in f:
+            fields = line.split(" ")
+            rgb_path = fields[0].replace("???","rgb")
+            rgb_path = rgb_path.replace("***","crop")
+            depth_path = fields[0].replace("???","surfnorm")
+            depth_path = depth_path.replace("***","depthcrop")
+            imageRGB = os.path.join(root,"ROD",rgb_path)
+            imagedepth = os.path.join(root,"ROD",depth_path)
+            label = int(fields[1])
+
+            if RAM:
+              self.images.append(((pil_loader(imageRGB), pil_loader(imagedepth)), label))
+            else:
+              self.images.append(((imageRGB,imagedepth),label))
+
+
+
+    def __getitem__(self, index):
+
+        images, label = self.images[index] 
+
+        if self.transform is not None:
+
+            if self.RAM:
+              image1 = self.transform(images[0])
+              image2 = self.transform(images[1])
+            else:
+              image1 = self.transform(pil_loader(images[0]))
+              image2 = self.transform(pil_loader(images[1]))
+
+        return image1,image2, label
+
+    def __len__(self):
+
+        length = len(self.images) 
+
+        return length
+  
