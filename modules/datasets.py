e9015050613d15d0e7a038e08d9a3a3e263bd2b8
Gabriele Goletto <48565490+ezius07@users.noreply.github.com>
2020-06-04 18:30:41 +0200

    RotatedDataset added

--- modules/datasets.py
+++ modules/datasets.py
@@ -3,6 +3,7 @@ from torchvision.datasets import VisionDataset
 from PIL import Image
 import os
 import os.path
+from .transforms import CoupledRotation
 
 
 def pil_loader(path):
@@ -11,34 +12,61 @@ def pil_loader(path):
         return img.convert('RGB')
 
 
-class SynROD(VisionDataset):
+class SynROD_ROD(VisionDataset):
 
-    def __init__(self, root, RAM, split=None, transforms=None):
-        super(SynROD, self).__init__(root, transforms=transforms)
+    def __init__(self, root, RAM=False, category=None, split=None, transforms=None):
+        super(SynROD_ROD, self).__init__(root, transforms=transforms)
 
         self.images = []
         self.RAM = RAM
-        if split not in ["train", "test"]:
-            raise ValueError("Split not acceptable!")
+       
+        if category not in ["ROD", "synROD"]:
+            raise ValueError("category not acceptable!")
+            
+        if category=="synROD": 
+            if split not in ["train", "test"]:
+                raise ValueError("split not acceptable!")
+        
+            basename = os.path.join(root, category, "synARID_50k-split_")
+            path1 = basename + "depth_" + split + "1.txt"
+            path2 = basename + "rgb_" + split + "1.txt"
 
-        basename = os.path.join(root, "synROD", "synARID_50k-split_")
-        path1 = basename + "depth_" + split + "1.txt"
-        path2 = basename + "rgb_" + split + "1.txt"
+            with open(path1, "r") as f1, open(path2, "r") as f2:
+                num_lines = sum(1 for line in open(path1, "r"))
+                pbar = tqdm.tqdm(total=num_lines, position=0, leave=True)
+                for line1, line2 in zip(f1, f2):
+                    pbar.update(1)
+                    fields1 = line1.split(" ")
+                    fields2 = line2.split(" ")
+                    imagedepth = os.path.join(root, "synROD", fields1[0])
+                    imageRGB = os.path.join(root, "synROD", fields2[0])
 
-        with open(path1, "r") as f1, open(path2, "r") as f2:
-            num_lines = sum(1 for line in open(path1, "r"))
-            pbar = tqdm.tqdm(total=num_lines, position=0, leave=True)
-            for line1, line2 in zip(f1, f2):
-                pbar.update(1)
-                fields1 = line1.split(" ")
-                fields2 = line2.split(" ")
-                imagedepth = os.path.join(root, "synROD", fields1[0])
-                imageRGB = os.path.join(root, "synROD", fields2[0])
+                    if RAM:
+                        self.images.append(((pil_loader(imageRGB), pil_loader(imagedepth)), int(fields1[1])))
+                    else:
+                        self.images.append(((imageRGB, imagedepth), int(fields1[1])))
+                        
+        else:
+            
+            filename = os.path.join(root, category, "rod-split_sync.txt")
+            with open(filename, "r") as f:
+                num_lines = sum(1 for _ in open(filename))
+                pbar = tqdm.tqdm(total=num_lines, position=0, leave=True)
+                for line in f:
+                    pbar.update(1)
+                    fields = line.split(" ")
+                    rgb_path = fields[0].replace("???", "rgb")
+                    rgb_path = rgb_path.replace("***", "crop")
+                    depth_path = fields[0].replace("???", "surfnorm")
+                    depth_path = depth_path.replace("***", "depthcrop")
+                    rgb_img = os.path.join(root, "ROD", rgb_path)
+                    depth_img = os.path.join(root, "ROD", depth_path)
+                    label = int(fields[1])
 
-                if RAM:
-                    self.images.append(((pil_loader(imageRGB), pil_loader(imagedepth)), int(fields1[1])))
-                else:
-                    self.images.append(((imageRGB, imagedepth), int(fields1[1])))
+                    if RAM:
+                        self.images.append(((pil_loader(rgb_img), pil_loader(depth_img)), label))
+                    else:
+                        self.images.append(((rgb_img, depth_img), label)) 
 
     def __getitem__(self, index):
 
@@ -61,51 +89,18 @@ class SynROD(VisionDataset):
 
         return length
 
+class RotatedDataset(SynROD_ROD):
+    r"""
+    Variation of a dataset (for relative rotation).
 
-class ROD(VisionDataset):
+    Arguments:
+        dataset (SynROD_ROD): The whole Dataset
+    """
+    def __init__(self, dataset):
+        self.dataset = dataset
 
-    def __init__(self, root, RAM, transforms=None):
-        super(ROD, self).__init__(root, transforms=transforms)
-
-        self.images = []
-        self.RAM = RAM
-        filename = os.path.join(root, "ROD", "rod-split_sync.txt")
-        with open(filename, "r") as f:
-            num_lines = sum(1 for _ in open(filename))
-            pbar = tqdm.tqdm(total=num_lines, position=0, leave=True)
-            for line in f:
-                pbar.update(1)
-                fields = line.split(" ")
-                rgb_path = fields[0].replace("???", "rgb")
-                rgb_path = rgb_path.replace("***", "crop")
-                depth_path = fields[0].replace("???", "surfnorm")
-                depth_path = depth_path.replace("***", "depthcrop")
-                rgb_img = os.path.join(root, "ROD", rgb_path)
-                depth_img = os.path.join(root, "ROD", depth_path)
-                label = int(fields[1])
-
-                if RAM:
-                    self.images.append(((pil_loader(rgb_img), pil_loader(depth_img)), label))
-                else:
-                    self.images.append(((rgb_img, depth_img), label))
-
-    def __getitem__(self, index):
-
-        (rgb_img, depth_img), label = self.images[index]
-
-        if self.transforms is not None:
-
-            if self.RAM:
-                rgb_img = self.transforms(rgb_img)
-                depth_img = self.transforms(depth_img)
-            else:
-                rgb_img = self.transforms(pil_loader(rgb_img))
-                depth_img = self.transforms(pil_loader(depth_img))
-
-        return rgb_img, depth_img, label
+    def __getitem__(self, idx):
+        return CoupledRotation()(self.dataset[idx][0], self.dataset[idx][1])
 
     def __len__(self):
-
-        length = len(self.images)
-
-        return length
+        return len(self.dataset)
