ea76fdf8efa58f889e7f9e1d503f02b8629af36a
Vittorio Zampinetti <47151363+toyo97@users.noreply.github.com>
2020-06-10 21:47:12 +0200

    implemented HAFNNet

--- modules/net.py
+++ modules/net.py
@@ -8,105 +8,12 @@ from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1
 
 model_urls = {'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth'}
 
-
-"""class LambdaRev(Function):
-    @staticmethod
-    def forward(ctx, x, lamda):
-        @param x: input
-        @param lamda: scalar to be multiplied for in the backprop pass
-        @return: output
-        ctx.lamda = lamda
-        return x.view_as(x)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        return grad_output.mul(ctx.lamda), None"""
-
-
 def init_weights(m):
     if type(m) in [nn.Linear, nn.Conv2d]:
         nn.init.xavier_uniform_(m.weight)
         m.bias.data.fill_(0.0)
 
-
-class OldNet(nn.Module):
-    def __init__(self, num_classes, single_mod=None):  # single_mod = 'RGB' or 'depth'
-        """
-        Old version of the RGBD domain adaptation network
-        @param num_classes:
-        @param single_mod:
-        """
-        super(OldNet, self).__init__()
-
-        self.single_mod = single_mod
-        self.num_maps = 1024
-        if self.single_mod is not None:
-            self.num_maps = 512
-
-        self.resnet18 = models.resnet18(pretrained=True)
-        self.rgb_feature_extractor = nn.Sequential(
-            *(list(copy.deepcopy(self.resnet18).children())[:-2]))  # Eliminate last fc and avg pool
-        self.depth_feature_extractor = nn.Sequential(
-            *(list(copy.deepcopy(self.resnet18).children())[:-2]))  # Eliminate last fc and avg pool
-
-        self.main_head = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),  # output has shape (batch_size, num_featues, 1, 1)
-            nn.Flatten(),
-            nn.Linear(self.num_maps, 1000),  # wants first dimension = batch_size
-            # In resnet18 it is 512 * block.expansion -> ???
-            nn.BatchNorm1d(num_features=1000),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(1000, num_classes),
-        )
-
-        # Xavier initialization
-        self.main_head.apply(init_weights)
-
-        self.pretext_head = nn.Sequential(
-            nn.Conv2d(in_channels=1024, out_channels=100, kernel_size=(1, 1), bias=True, stride=1, padding=0),
-            # output img = 100 channels of 7x7
-            nn.BatchNorm2d(num_features=100),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=100, out_channels=100, kernel_size=(3, 3), bias=True, stride=2, padding=0),
-            # output img = 100 channels of 98x98
-            nn.BatchNorm2d(num_features=100),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(900, 100),
-            nn.BatchNorm1d(num_features=100),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(100, 4),
-        )
-
-        self.pretext_head.apply(init_weights)
-
-    def forward(self, x=None, y=None, lamda=None):  # x is the rgb batch, y the depth batch
-        if self.single_mod == 'RGB':
-            tot_features = self.rgb_feature_extractor(x)
-
-        if self.single_mod == 'depth':
-            tot_features = self.depth_feature_extractor(y)
-
-        if self.single_mod is None:
-            rgb_features = self.rgb_feature_extractor(x)  # list of rgb filters of the batch (list_size = batch_size)
-            depth_features = self.depth_feature_extractor(y)
-            tot_features = torch.cat((depth_features, rgb_features), 1)
-            # size_allFeatures = (batch_size, number_filters, height, width)
-            # number_filters = 512
-            # height = width = 7 if input of network is 224x224
-
-        if lamda is None:
-            class_scores = self.main_head(tot_features)  # class scores of the batch
-            return class_scores
-
-        else:
-            out2 = self.pretext_head(tot_features)
-            out2 = LambdaRev.apply(out2, lamda)  # lambda mul in backward pass
-            return out2
-
-
+        
 class ModifiedResNet(nn.Module):
     """
     Pytorch implementation of Resnet without the last two layers (fully connected and pooling)
@@ -244,7 +151,7 @@ class Net(nn.Module):
             nn.BatchNorm1d(num_features=1000),
             nn.ReLU(),
             nn.Dropout(p=0.5),
-            nn.Linear(1000, num_classes),
+            nn.Linear(1000, num_classes)
         )
 
         # Xavier initialization
@@ -264,7 +171,7 @@ class Net(nn.Module):
             nn.BatchNorm1d(num_features=100),
             nn.ReLU(),
             nn.Dropout(p=0.5),
-            nn.Linear(100, 4),
+            nn.Linear(100, 4)
         )
 
         self.pretext_head.apply(init_weights)
@@ -284,6 +191,94 @@ class Net(nn.Module):
         if pretext is None:
             class_scores = self.main_head(tot_features)
             return class_scores
+
+        else:
+            out2 = self.pretext_head(tot_features)
+            return out2
+
+class HAFNNet(nn.Module):
+    def __init__(self, num_classes, single_mod=None, dropout_p=0.5):
+        """
+        RGBD Domain Adaptation network based on Resnet18
+        @param num_classes: number of output classes
+        @param single_mod: specify `RGB` or `depth` if only one modality is being passed through the network
+                        otherwise leave it to None
+        """
+        super(HAFNNet, self).__init__()
+
+        if single_mod not in ['RGB', 'depth', None]:
+            raise ValueError('single_mod parameter not valid. Please choose between `RGB` or `depth`, otherwise leave '
+                             'it to None')
+
+        self.single_mod = single_mod
+        self.dropout_p = dropout_p
+        num_maps = 1024
+        if self.single_mod is not None:
+            num_maps = 512
+
+        state_dict = load_state_dict_from_url(model_urls['resnet18'])
+
+        self.rgb_feature_extractor = ModifiedResNet(BasicBlock, [2, 2, 2, 2])
+        self.rgb_feature_extractor.load_state_dict(state_dict, strict=False)
+        self.depth_feature_extractor = ModifiedResNet(BasicBlock, [2, 2, 2, 2])
+        self.depth_feature_extractor.load_state_dict(state_dict, strict=False)
+
+        self.main_head = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),  # output has shape (batch_size, num_featues, 1, 1)
+            nn.Flatten(),
+            nn.Linear(num_maps, 1000),  # wants first dimension = batch_size
+            # In resnet18 it is 512 * block.expansion -> ???
+            nn.BatchNorm1d(num_features=1000),
+            nn.ReLU(),
+            nn.Dropout(p=self.dropout_p)
+        )
+        self.fc2 = nn.Linear(1000, num_classes)
+
+
+        # Xavier initialization
+        self.main_head.apply(init_weights)
+        self.fc2.apply(init_weights)
+
+        self.pretext_head = nn.Sequential(
+            nn.Conv2d(in_channels=1024, out_channels=100, kernel_size=(1, 1), bias=True, stride=1, padding=0),
+            # output img = 100 channels of 7x7
+            nn.BatchNorm2d(num_features=100),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=100, out_channels=100, kernel_size=(3, 3), bias=True, stride=2, padding=0),
+            # output img = 100 channels of 98x98
+            nn.BatchNorm2d(num_features=100),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(900, 100),
+            nn.BatchNorm1d(num_features=100),
+            nn.ReLU(),
+            nn.Dropout(p=self.dropout_p),
+            nn.Linear(100, 4)
+        )
+
+        self.pretext_head.apply(init_weights)
+
+
+    def forward(self, x=None, y=None, pretext=None):  # x is the rgb batch, y the depth batch
+        if self.single_mod == 'RGB':
+            tot_features = self.rgb_feature_extractor(x)
+
+        if self.single_mod == 'depth':
+            tot_features = self.depth_feature_extractor(y)
+
+        if self.single_mod is None:
+            rgb_features = self.rgb_feature_extractor(x)  # list of rgb filters of the batch (list_size = batch_size)
+            depth_features = self.depth_feature_extractor(y)
+            tot_features = torch.cat((depth_features, rgb_features), 1)
+
+        if pretext is None:
+            out = self.main_head(tot_features)
+
+            if self.training:
+           		out.mul_(math.sqrt(1 - self.dropout_p))
+            
+            class_scores = self.fc2(out)
+            return out, class_scores
 
         else:
             out2 = self.pretext_head(tot_features)
